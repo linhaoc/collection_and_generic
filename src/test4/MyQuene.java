f236b9964c02e5b5983a046b7ffed3a65a7ceca6
HP <HP@10.0.2.237>
2019-05-01 18:23:19 +0800

    1

--- src/test4/MyQuene.java
+++ src/test4/MyQuene.java
@@ -1,5 +1,26 @@
 package test4;
 
-public class MyQuene {
+import java.util.LinkedList;
 
+public class MyQuene {
+	LinkedList lists = new LinkedList();
+	public void push(Object obj) {
+		lists.addLast(obj);
+	}
+	public Object pop() {
+		return lists.removeFirst();
+	}
+	public String toString() {
+		return lists.toString();
+	}
+	public static void main(String[] args) {
+		MyQuene mq = new MyQuene();
+		mq.push("ºìÉ«");
+		mq.push("³ÈÉ«");
+		mq.push("À¶É«");
+		System.out.println(mq);
+		System.out.println(mq.pop());
+		System.out.println(mq);
+		
+	}
 }
