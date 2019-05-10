90d56b49792259954ed0b5fe149c300550304ff9
HP <HP@LAPTOP-9GM69S8D.lan>
2019-05-11 00:13:22 +0800

    1

--- src/test3/map_tset.java
+++ src/test3/map_tset.java
@@ -2,31 +2,41 @@ package test3;
 
 import java.util.*;
 
-class candidate{
+class candidate {
 	private int num;
 	private String name;
 	public candidate(int num ,String name) {
 		this.num = num;
 		this.name = name;
-	}
+	}	
+	
 }
 public class map_tset {
 	public static void main(String[] args) {
-		TreeMap map = new TreeMap();
-		map.put(new candidate(1,"张三"), 0);
-		map.put(new candidate(2,"李四"),0);
-		map.put(new candidate(3,"王五"),0);
+		HashMap map = new HashMap();
+		//定义候选人
+		candidate c1 = new candidate(1,"张三");
+		candidate c2 = new candidate(2,"李四");
+		candidate c3 = new candidate(3,"王五");
+		//创建HashMap对象
+		map.put(1, 0);
+		map.put(2,0);
+		map.put(3,0);
 	
-			Scanner sc = new Scanner(System.in);
-			System.out.println("请输入你要选举的人的编号如(1,2,3):");
-			String temp = sc.nextLine();
-			String []t = temp.split(",");
-			int []num = null;
-			for(int i=0;i<2;i++) {
-				num[i]= Integer.parseInt(t[i]);
-			}
-		
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("请输入十个选举人的提交的候选人编号(以空格隔开):");
+		String temp = sc.nextLine();
+		String []t = temp.split(" ");
+		int []num = new int [20];
+		for(int i=0;i<t.length;i++) {
+			num[i]= Integer.parseInt(t[i]);
+		}
+		for(int i=0;i<num.length;i++) {
+			Integer j = (Integer) map.get(num[i]);
+			if(map.containsKey(num[i]))
+				map.put(num[i],++j);
+		}
+		map.forEach((key,value)->System.out.println("候选人"+key+"号票数为:"+value+" "));
 	}
 	
 }
